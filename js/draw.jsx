1d173a290a353f18a871d6cb227f19ce00a5b942
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-11-07 00:31:51 +0100

    add Deno dev server

---
+++ js/draw.jsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { useRef, useState } from "react";
+import Excalidraw from "@excalidraw/excalidraw";
+import InitialData from "../data/initial-data.js";
+
+function Draw() {
+	const excalidrawRef = useRef(null);
+
+	const [viewModeEnabled, setViewModeEnabled] = useState(true);
+	const [zenModeEnabled, setZenModeEnabled] = useState(true);
+	const [gridModeEnabled, setGridModeEnabled] = useState(false);
+
+	return (
+		<div className="excalidraw-wrapper">
+			<Excalidraw.default
+				ref={excalidrawRef}
+				initialData={InitialData}
+				onChange={(elements, state) =>
+					console.log(
+						"Elements :",
+						elements,
+						"State : ",
+						state,
+					)}
+				onPointerUpdate={(payload) => console.log(payload)}
+				onCollabButtonClick={() =>
+					window.alert("You clicked on collab button")}
+				viewModeEnabled={viewModeEnabled}
+				zenModeEnabled={zenModeEnabled}
+				gridModeEnabled={gridModeEnabled}
+			/>
+		</div>
+	);
+}
+export default Draw;
