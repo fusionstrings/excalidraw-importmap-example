32646b0efb3bdf139a1198962fd73f5b2dc24ff3
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-11-07 13:46:18 +0100

    use Deno.readTextFile

--- deploy-server.js
+++ deploy-server.js
@@ -5,7 +5,8 @@ async function handler(_req) {
   // of the repository to explore the available methods.
 
   // Relative paths are relative to the root of the repository
-  const readmeRelative = await Deno.readFile("./README.md");
+  // const readmeRelative = await Deno.readFile("./README.md");
+  const readmeRelative = await Deno.readTextFile("./README.md");
   // Absolute paths.
   // The content of the repository is available under at Deno.cwd().
   // const readmeAbsolute = await Deno.readFile(`${Deno.cwd()}/README.md`);
@@ -15,8 +16,8 @@ async function handler(_req) {
 //   );
 
   // Decode the Uint8Array as string.
-  const readme = new TextDecoder().decode(readmeRelative);
-  return new Response(readme);
+  // const readme = new TextDecoder().decode(readmeRelative);
+  return new Response(readmeRelative);
 }
 
 console.log("Listening on http://localhost:8080");
