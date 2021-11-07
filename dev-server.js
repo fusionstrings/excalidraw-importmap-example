83d554d2f81e1f8e2ae57c449f8fb9740f52ed07
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-11-07 01:18:44 +0100

    Update dev-server.js

--- dev-server.js
+++ dev-server.js
@@ -1,5 +1,3 @@
-// @ts-check
-
 import { listenAndServe } from "https://deno.land/std@0.113.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.113.0/http/file_server.ts";
 import {
