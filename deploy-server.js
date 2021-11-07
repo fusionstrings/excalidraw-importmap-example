50281509d1ecef94a4f9ad27a47b5877f1d9d2ef
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-11-07 13:52:49 +0100

    use assetMap

--- deploy-server.js
+++ deploy-server.js
@@ -1,12 +1,19 @@
 import { listenAndServe } from "https://deno.land/std@0.111.0/http/server.ts";
 
-async function handler(_req) {
+const assetMap = {
+    '/': './deploy.html',
+    '/README.md': './README.md'
+}
+
+async function handler(request) {
+    const { pathname } = new URL(request.url);
   // Let's read the README.md file available at the root
   // of the repository to explore the available methods.
 
   // Relative paths are relative to the root of the repository
   // const readmeRelative = await Deno.readFile("./README.md");
-  const readmeRelative = await Deno.readTextFile("./README.md");
+  const assetPath = assetMap[pathname]
+  const testFile = await Deno.readTextFile(assetPath);
   // Absolute paths.
   // The content of the repository is available under at Deno.cwd().
   // const readmeAbsolute = await Deno.readFile(`${Deno.cwd()}/README.md`);
@@ -17,7 +24,7 @@ async function handler(_req) {
 
   // Decode the Uint8Array as string.
   // const readme = new TextDecoder().decode(readmeRelative);
-  return new Response(readmeRelative);
+  return new Response(testFile);
 }
 
 console.log("Listening on http://localhost:8080");
