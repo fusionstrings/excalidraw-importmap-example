74128dbce590ed8259341303bcc4028a52968bb2
Dilip Kr. Shukla <fusionstrings@gmail.com>
2021-11-17 12:39:00 +0100

    add importmap generator

---
+++ js/importmap-generator.js
@@ -0,0 +1,22 @@
+import { Generator } from "https://cdn.jsdelivr.net/gh/fusionstrings/dependencies@36513823bc98061f2f84e1c3fd6352f711146aa2/dist/deno/jspm.js";
+
+async function main(subpath = "./js/main.js") {
+    const generator = new Generator();
+
+    await generator.install([
+        {
+            alias: "@fusionstrings/river",
+            target: "./",
+            subpath,
+        },
+    ]);
+    const importMap = JSON.stringify(generator.getMap(), null, 2);
+    return importMap;
+}
+
+if (import.meta.main) {
+    const importmap = await main();
+    console.log('importmap: ', importmap);
+}
+
+export { main }
